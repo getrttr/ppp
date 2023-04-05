2cfd1a17b967b9547067f62d6fcc2d3ca42cd0a3
FuckCray <51244165+getrttr@users.noreply.github.com>
2023-04-05 18:13:58 +0300

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from products.views import index, products,context
-
+from products.views import index, products, email, login, profile, register
 urlpatterns = {
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products'),
-    path('security/', context, name='security')
-    
+    path('email/', email, name='email'),
+    path('login/', login, name='login'),
+    path('profile/', profile, name='profile'),
+    path('register/', register, name='register'),
+
 }
